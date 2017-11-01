de26d5e12a58dc27869bcebb4c500083b669e118
FulcrandG <guilhem.fulcrand@epitech.eu>
2017-11-01 16:24:46 +0100

    Base setup for server

---
+++ server/include/info.h
@@ -0,0 +1,51 @@
+
+/*
+** main.c for Server in /home/alexandre/Documents/git/fileTransfert/server/src
+**
+** Made by alexandre Chamard-bois
+** Login   <alexandre@epitech.net>
+**
+** Started on  Wed Nov 01 14:52:24 2017 alexandre Chamard-bois
+** Last update Wed Nov 01 14:52:40 2017 alexandre Chamard-bois
+*/
+
+/* Usefull defines */
+
+#ifndef EXIT_S
+# define EXIT_S         0
+#endif
+
+#ifndef EXIT_F
+# define EXIT_F         42
+#endif
+
+#ifndef STD_F
+# define STD_F          -1
+#endif
+
+#ifndef TRY
+# define TRY(x, y)      (x == y ? perror(NULL), exit(EXIT_F) : 0)
+#endif
+
+#ifndef DEFAULT_PORT
+# define DEFAULT_PORT   1234       // Should only use port number >=1024, others may be in use
+#endif
+
+#ifndef BUFF_SIZE
+# define BUFF_SIZE      1024       // 1Ko for now, to discuss later
+#endif
+
+#ifndef TIMEOUT
+# define TIMEOUT        -1         // timeout for server, -1 for disabling it
+#endif
+
+/* Usefull structs */
+
+typedef struct                  s_server_info {
+        struct sockaddr_in      addr;
+        socklen_t               addrlen;
+        int                     fd;
+        int                     sockfd;
+        int                     msg_len;
+        char                    msg_buff[BUFF_SIZE];
+}                               t_server_info;
--- server/src/main.c
+++ server/src/main.c
@@ -8,12 +8,33 @@
 ** Last update Wed Nov 01 14:52:40 2017 alexandre Chamard-bois
 */
 
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-int main(int ac, char **av)
+#include "info.h"
+
+void    launch_server(t_server_info *info)
 {
+        TRY((info->fd = socket(AF_INET, SOCK_STREAM, 0)), STD_F);
+}
+
+void    close_server(t_server_info *info)
+{
+        close(info->fd);
+}
+
+int                     main(int ac, char **av)
+{
+        t_server_info   info;
 	(void)ac;
 	(void)av;
 	printf("server\n");
-	return (0);
+        launch_server(&info);
+        close_server(&info);
+	return (EXIT_S);
 }
